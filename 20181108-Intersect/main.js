6cd8f9c9b82cbaaee6a6a276be014db8c7a956d6
jjnotjayjay <jj@alumni.usc.edu>
2018-11-08 15:55:31 -0800

    Implemented filter login in the reduce function, removed unnecessary variable.

--- 20181108-Intersect/main.js
+++ 20181108-Intersect/main.js
@@ -9,6 +9,7 @@ function intersect(...arrays) {
     }
   })
 
-  const counts = Object.entries(countValues)
-  return counts.filter(el => el[1] === arrays.length).reduce((cum, next) => cum.concat(next[0]), [])
+  return Object.entries(countValues).reduce((cum, next) => next[1] === arrays.length ? cum.concat(next[0]) : cum, [])
 }
+
+console.log(intersect(['foo', 'bar', 29, 1, 38, 19, 249], ['foo', 38, 2, 19, 3, 20], [1,2,3,4,38,'foo', 38, 19]))
