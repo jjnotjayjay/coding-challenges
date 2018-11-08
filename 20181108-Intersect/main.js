c2cd7c8b529b40d99ed6494ba149bb2121717216
jjnotjayjay <jj@alumni.usc.edu>
2018-11-08 15:56:51 -0800

    Removed test function.

--- 20181108-Intersect/main.js
+++ 20181108-Intersect/main.js
@@ -11,5 +11,3 @@ function intersect(...arrays) {
 
   return Object.entries(countValues).reduce((cum, next) => next[1] === arrays.length ? cum.concat(next[0]) : cum, [])
 }
-
-console.log(intersect(['foo', 'bar', 29, 1, 38, 19, 249], ['foo', 38, 2, 19, 3, 20], [1,2,3,4,38,'foo', 38, 19]))
