2bf2d9c7814adf69ab7499ec2bd6e0d3be6387c8
jjnotjayjay <jj@alumni.usc.edu>
2018-11-12 14:39:15 -0800

    Committing index.html and added script logic to main.js

--- 20181112-Recursive-Add/main.js
+++ 20181112-Recursive-Add/main.js
@@ -2,3 +2,11 @@ function recursiveAdd(n) {
   if (n === 0) return n
   return n + recursiveAdd(n - 1)
 }
+
+const $input = document.getElementById('user-input')
+const $submit = document.getElementById('submit')
+const $output = document.getElementById('output')
+
+$submit.addEventListener('click', () => {
+  $output.textContent = recursiveAdd($input.valueAsNumber)
+})
