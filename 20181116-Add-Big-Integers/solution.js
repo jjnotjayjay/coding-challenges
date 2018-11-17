4ff8b902c51fc81d35772d289ac41da83442d962
jjnotjayjay <jj@alumni.usc.edu>
2018-11-16 17:32:58 -0800

    Committing solution.js

---
+++ 20181116-Add-Big-Integers/solution.js
@@ -0,0 +1,17 @@
+function sumStrings(a,b) {
+  const aDigits = a.split('')
+  const bDigits = b.split('')
+  const sumDigits = []
+  let carry = 0
+
+  while (aDigits.length || bDigits.length) {
+    let sum = (parseInt(aDigits.pop()) || 0) + (parseInt(bDigits.pop()) || 0) + carry
+    sumDigits.unshift(sum % 10)
+    sum >= 10
+      ? carry = 1
+      : carry = 0
+    if (!aDigits.length && !bDigits.length && carry) sumDigits.unshift(1)
+  }
+
+  return sumDigits.join('').replace(/^0+/g, '')
+}
