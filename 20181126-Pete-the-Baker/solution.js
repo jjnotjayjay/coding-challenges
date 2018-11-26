70c2ebb33e93c3ad7be0971771eaffc9e69453cd
jjnotjayjay <jj@alumni.usc.edu>
2018-11-26 15:09:21 -0800

    Removed unnecessary ternary from solution.js

--- 20181126-Pete-the-Baker/solution.js
+++ 20181126-Pete-the-Baker/solution.js
@@ -1,7 +1,5 @@
 function cakes(recipe, available) {
   return Object.keys(recipe).reduce((maxCakes, ingredient) => {
-    return available[ingredient]
-      ? Math.min(maxCakes, Math.floor(available[ingredient] / recipe[ingredient]))
-      : 0
+    return Math.min(Math.floor(available[ingredient] / recipe[ingredient]) || 0, maxCakes)
   }, Infinity)
 }
