d5be9fc2dc9e261a0355ed2e1efe55161b1f5db9
jjnotjayjay <jj@alumni.usc.edu>
2018-11-26 16:07:08 -0800

    Replaced squaring by simple multiplication with the more performant Math.pow and removed console.log

--- 20181126-K-Closest-Points/solution.js
+++ 20181126-K-Closest-Points/solution.js
@@ -1,5 +1,3 @@
 function closestPoints(k, points) {
-  return points.sort((a, b) => (a[0] * a[0] + a[1] * a[1]) - (b[0] * b[0] + b[1] * b[1])).slice(0, k)
+  return points.sort((a, b) => (Math.pow(a[0], 2) + Math.pow(a[1], 2)) - (Math.pow(b[0], 2) + Math.pow(b[1], 2))).slice(0, k)
 }
-
-console.log(closestPoints(2, [[-2, 4], [0, -2], [-1, 0], [3, 5], [-2, -3], [3, 2]]))
