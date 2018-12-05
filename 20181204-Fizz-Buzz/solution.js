35092ea447b6db96fd3e5328ace99be33f327968
jjnotjayjay <jj@alumni.usc.edu>
2018-12-04 17:55:01 -0800

    Committing initial solution.js

---
+++ 20181204-Fizz-Buzz/solution.js
@@ -0,0 +1,10 @@
+function fizzbuzz(n) {
+  let output = []
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) output.push('FizzBuzz')
+    else if (i % 3 === 0) output.push('Fizz')
+    else if (i % 5 === 0) output.push('Buzz')
+    else output.push(i)
+  }
+  return output
+}
